31ebd6c45073ddd6740c24115270522c10fc3e85
martabakhoror <jumanurrohman.com>
2021-10-07 16:32:15 +0700

    pertemuan1

---
+++ 1-2/pertemuan.py
@@ -0,0 +1,31 @@
+#string
+a= "python pemula"
+print (type(a))
+#menyatakan karakter/kalimat bisa berupa huruf angka, dll diapit tanda " atau '
+
+#integer
+print (20)
+#menyatakan bilangan bulat
+
+#float
+print (3.14)
+#menyatakan bilangan yang mempunyai koma 
+
+#list
+angka =[1,2,3,4,5]
+print (angka)
+print ('satu', 'dua', 'tiga')
+
+#tuple
+print ((1,2,3,4,5))
+print (("satu", "dua", "tiga"))
+
+#dictionary
+biodata = {
+    "nama"      : "jumanur rohman",
+    "nim"       : "181112101",
+    "jurusan"   : "komputer"
+}
+
+print (biodata)
+print (type (biodata))
